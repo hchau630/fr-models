5a0a798212edfa4c81d37ca12592f556eb9e3bd1
Ho Yin Chau <hoyinchau@Hos-MacBook-Pro-2.local>
2023-03-07 16:07:39 -0500

    v0.1.10: removed unnecessary assert.

--- fr_models/constraints.py
+++ fr_models/constraints.py
@@ -79,15 +79,6 @@ class ParadoxicalCon(Constraint):
         
         sub_lp_model = nmd.LinearizedMultiCellSSNModel(sub_W, sub_r_star, lp_model.w_dims)
         
-        assert sub_lp_model.W.is_cuda
         result = sub_lp_model.instability(**self.kwargs) - self.min_subcircuit_instability
-        
-#         subcircuit_cell_types = [i for i in range(r_model.a_model.n) if i != self.cell_type]
-#         subcircuit_model = r_model.a_model.sub_model(subcircuit_cell_types)
-#         subcircuit_r_star = r_model.r_star[torch.tensor(subcircuit_cell_types)]
-#         sub_lp_model = subcircuit_model.numerical_model(r_model.grid).linear_perturbed_model(subcircuit_r_star, share_mem=True)
-        
-#         assert sub_lp_model.W.is_cuda
-#         result = sub_lp_model.instability(**self.kwargs) - self.min_subcircuit_instability
         
         return result
